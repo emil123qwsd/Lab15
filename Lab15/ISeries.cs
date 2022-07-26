230689ef66f8f6a61e8167387e7e3d4ae221ff7b
эмиль ахметханов <emil.ah@yandex.ru>
2022-07-26 23:22:48 +0300

    Добавьте файлы проекта.

---
+++ Lab15/ISeries.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    /*
+     * метод void SetStart(int x) - устанавливает начальное значение
+     метод int GetNext() - возвращает следующее число ряда
+     метод void Reset() - выполняет сброс к начальному значению
+     */
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void reset();
+    }
+    class ArithProgression : ISeries
+    {
+        int step;
+        int startValue;
+        int currentValue;
+        public int GetNext()
+        {
+            currentValue+=step;
+            return currentValue;
+        }
+
+        public void reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue=x;
+            currentValue = startValue;
+        }
+        public void setStep(int s)
+        {
+            step = s;
+        }
+    }
+    class GeoProgression : ISeries
+    {
+        int step;
+        int startValue;
+        int currentValue;
+        public int GetNext()
+        {
+            currentValue *= step;
+            return currentValue;
+        }
+
+        public void reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void setStep(int s)
+        {
+            step = s;
+        }
+    }
+}
---
+++ Lab15/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arith = new ArithProgression();
+            Console.WriteLine("Введите первый член арифметической прогресии");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            arith.SetStart(a1);
+            Console.WriteLine("Введите шаг арифметической прогресии");
+            int an = Convert.ToInt32(Console.ReadLine());
+            arith.setStep(an);
+            int counter = 1;
+            Console.WriteLine("Введите число членов арифметической прогресии");
+            int N = Convert.ToInt32(Console.ReadLine());
+            while (counter <= N) 
+            {
+                Console.WriteLine(arith.GetNext());
+                counter++;
+            }
+            //Console.WriteLine(arith.GetNext());
+            //Console.WriteLine(arith.GetNext());
+            //Console.WriteLine(arith.GetNext());
+            arith.reset();
+            Console.WriteLine(arith.GetNext());
+            Console.WriteLine(arith.GetNext());
+
+            GeoProgression geo = new GeoProgression();
+            Console.WriteLine("Введите первый член геометрической прогресии");
+            int g1 = Convert.ToInt32(Console.ReadLine());
+            geo.SetStart(g1);
+            Console.WriteLine("Введите шаг геометрической прогресии");
+            int gn = Convert.ToInt32(Console.ReadLine());
+            geo.setStep(gn);
+            int counter2 = 1;
+            Console.WriteLine("Введите число членов арифметической прогресии");
+            int N2 = Convert.ToInt32(Console.ReadLine());
+            while (counter2 <= N2)
+            {
+                Console.WriteLine(geo.GetNext());
+                counter2++;
+            }
+            //Console.WriteLine(geo.GetNext());
+            //Console.WriteLine(geo.GetNext());
+            //Console.WriteLine(geo.GetNext());
+            geo.reset();
+            Console.WriteLine(geo.GetNext());
+            Console.WriteLine(geo.GetNext());
+            Console.ReadKey();
+        }
+    }
+}
